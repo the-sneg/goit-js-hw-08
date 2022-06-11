911fff47293025f834825fa034cde5f0f646e0e4
the-sneg <the.sneg.sasha@gmail.com>
2022-06-11 17:58:56 +0300

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,24 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const markupGallery = createMarkupGallery(galleryItems);
 
-// instanceof
+galleryList.insertAdjacentHTML('beforeend', markupGallery);
+
+function createMarkupGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" style="display: block" />
+        </a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
